a446730b0353f8199d5d6a2ec84db0e711cb1ae9
xytorenok <xytorenok@gmail.com>
2023-05-07 20:23:25 +0300

    1

--- js/main.js
+++ js/main.js
@@ -30,3 +30,52 @@ window.onscroll = () => {
 //     block.classList.add('active')
 //   })
 // }
+
+
+const topLeftImg = document.querySelectorAll('.top-left-img');
+const topRightImg = document.querySelectorAll('.top-right-img');
+const bottomRightImg = document.querySelectorAll('.bottom-right-img');
+const bottomLeftImg = document.querySelectorAll('.bottom-left-img');
+
+window.addEventListener('mousemove', function (e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;
+
+
+  for (const elm of topLeftImg) {
+    elm.style.transform = `translate(-${x * 90}px, -${y * 60}px)`;
+  }
+
+  for (const elm of topRightImg) {
+    elm.style.transform = `translate(-${x * 40}px, -${y * 40}px)`;
+  }
+
+  for (const elm of bottomRightImg) {
+    elm.style.transform = `translate(-${x * 250}px, -${y * 70}px)`;
+  }
+
+  for (const elm of bottomLeftImg) {
+    elm.style.transform = `translate(-${x * 110}px, -${y * 110}px)`;
+  }
+
+
+});
+
+
+const btnProduct = document.querySelectorAll('.sect-3-product-btn');
+
+
+
+for (const btn of btnProduct) {
+  btn.onclick = function() {
+    for (const btn of btnProduct) {
+      btn.classList.remove('btn-active');
+    }
+        
+    this.classList.add('btn-active');
+  };
+
+
+}
+
+ 
