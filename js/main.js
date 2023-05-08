1960f4d0d0010e153e7c9d62950095409eb35151
xytorenok <xytorenok@gmail.com>
2023-05-08 19:01:57 +0300

    s

--- js/main.js
+++ js/main.js
@@ -61,21 +61,96 @@ window.addEventListener('mousemove', function (e) {
 
 });
 
+// ---------------section-3----------------------
 
 const btnProduct = document.querySelectorAll('.sect-3-product-btn');
-
-
+const liProduct = document.querySelectorAll('.sect-3-product-item');  
 
 for (const btn of btnProduct) {
-  btn.onclick = function() {
-    for (const btn of btnProduct) {
-      btn.classList.remove('btn-active');
-    }
-        
-    this.classList.add('btn-active');
-  };
+  btn.onclick = rollProduct
+}
 
+function rollProduct() {
+  for (const btn of btnProduct) {
+    btn.classList.remove('btn-active');
+  }
+
+  this.classList.add('btn-active');
+  count = Array.from(btnProduct).indexOf(this)
+  rollSlider()
+}
+
+
+
+// ---------------section-4----------------------
+const sliderWrapper = document.querySelector('.slider-wrapper')
+const sliderLine = document.querySelector('.slider-line')
+const slides = document.querySelectorAll('.section-4 .slide')
+
+const sliderBtnPrev = document.querySelector('.slider-button-prev')
+const sliderBtnNext = document.querySelector('.slider-button-next')
+
+let count = 0
+let width
+
+init()
+window.onresize = init
+
+function init() {
+  console.log('resize')
+  width = sliderWrapper.offsetWidth
+  sliderLine.style.width = width * slides.length + 'px'
+  slides.forEach(slide => {
+    slide.style.width = width + 'px'
+  });
+  rollSlider()
 
 }
 
- 
+
+
+
+sliderBtnPrev.onclick = () => {
+  count--
+  if (count < 0) {
+    count = slides.length - 1
+  }
+  rollSlider()
+  rollPrevProduct()
+
+}
+
+sliderBtnNext.onclick = () => {
+  count++
+  if (count >= slides.length) {
+    count = 0
+  }
+  rollSlider()
+  rollNextProduct()
+
+}
+
+function rollSlider() {
+  sliderLine.style.transform = `translate(-${count * width}px)`
+}
+
+function rollPrevProduct(){
+  const currentBtn = document.querySelector('.sect-3-product-btn.btn-active')
+  const currentLi = currentBtn.closest('.sect-3-product-item')
+  const prevLi = currentLi.previousElementSibling || liProduct[liProduct.length - 1]
+  const prevBtn = prevLi.querySelector('.sect-3-product-btn');
+
+
+  currentBtn.classList.remove('btn-active');
+  prevBtn.classList.add('btn-active');
+}
+
+function rollNextProduct(){
+  const currentBtn = document.querySelector('.sect-3-product-btn.btn-active')
+  const currentLi = currentBtn.closest('.sect-3-product-item')
+  const nextLi = currentLi.nextElementSibling || liProduct[0]
+  const nextBtn = nextLi.querySelector('.sect-3-product-btn');
+
+  currentBtn.classList.remove('btn-active');
+  nextBtn.classList.add('btn-active');
+}
